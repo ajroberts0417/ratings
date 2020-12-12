74b81433b2c03fa1f17f98181c13e50eed007d48
ajroberts0417 <ajroberts0417@gmail.com>
2020-12-11 18:07:21 -0800

    first working commit

---
+++ ratings/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ratings.users.models import User
+from ratings.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
